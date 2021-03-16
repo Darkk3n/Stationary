a1d93e38263c098bfaac6e1b332ef934ee55c96c
Gerardo <gerardo.aguilar01@outlook.com>
2021-03-15 22:57:45 -0600

    Setup API project

--- Darkk3nDevs.Stationary.Server/Startup.cs
+++ Darkk3nDevs.Stationary.Server/Startup.cs
@@ -1,15 +1,11 @@
+using System.Reflection;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace Darkk3nDevs.Stationary.Server
 {
@@ -26,6 +22,25 @@ namespace Darkk3nDevs.Stationary.Server
       public void ConfigureServices(IServiceCollection services)
       {
          services.AddControllers();
+
+         services.AddMediatR(typeof(Startup).GetTypeInfo().Assembly);
+
+         services.AddSwaggerGen(c =>
+         {
+
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+               Version = "v1",
+               Title = "Stationary API",
+               Description = "Stationary ASP.NET Core Web API",
+               Contact = new OpenApiContact
+               {
+                  Name = "Darkk3nDevs.Stationary.API",
+                  Email = "gerardo.aguilar01@outlook.com"
+               }
+            });
+
+         });
       }
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +60,14 @@ namespace Darkk3nDevs.Stationary.Server
          app.UseEndpoints(endpoints =>
          {
             endpoints.MapControllers();
+         });
+
+         app.UseSwagger();
+
+         app.UseSwaggerUI(c =>
+         {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            c.InjectStylesheet("/swagger/custom.css");
          });
       }
    }
